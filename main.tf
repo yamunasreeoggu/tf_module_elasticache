f7e4d0becfdab93a15a26d6919189eb764116cc9
yamunasreeoggu <yamunasree321@gmail.com>
2023-11-20 18:05:33 -0500

    D75 | Yamuna| added Elasticache module code

---
+++ main.tf
@@ -0,0 +1,45 @@
+ resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}-sg"
+  description = "${var.env}-${var.component}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "ELASTICACHE"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.component}-sg"
+  }
+}
+
+resource "aws_elasticache_subnet_group" "main" {
+  name       = "${var.env}-${var.component}"
+  subnet_ids = var.subnets
+
+  tags = {
+    Name = "${var.env}-${var.component}"
+  }
+}
+
+ resource "aws_elasticache_cluster" "main" {
+   cluster_id           = "${var.env}-${var.component}"
+   engine               = "redis"
+   node_type            = var.ec_node_type
+   num_cache_nodes      = var.ec_node_count
+   parameter_group_name = "default.redis6.x"
+   engine_version       = "6.2"
+   port                 = 6379
+   subnet_group_name    = aws_elasticache_subnet_group.main.name
+   security_group_ids = [aws_security_group.main.id]
+ }
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "component" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "subnets" {}
+variable "kms_key_id" {}
+variable "ec_node_type" {}
+variable "ec_node_count" {}
